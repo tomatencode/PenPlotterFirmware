3a5818b6f08c90cb00c29efb5e7ceaafbec33118
tomatencode <ingo.thomas@gmx.com>
2026-06-16 19:28:12 +0200

    v2 pcb pinss

--- src/config/PinsConfig.hpp
+++ src/config/PinsConfig.hpp
@@ -2,11 +2,11 @@
 #include <cstdint>
 
 // Motor PinsConfig
-constexpr uint8_t STEP_PIN_A = 19;
-constexpr uint8_t DIR_PIN_A  = 4;
-constexpr uint8_t STEP_PIN_B = 18;
-constexpr uint8_t DIR_PIN_B  = 5;
-constexpr uint8_t EN_PIN     = 23;
+constexpr uint8_t STEP_PIN_A = 27;
+constexpr uint8_t DIR_PIN_A  = 26;
+constexpr uint8_t STEP_PIN_B = 33;
+constexpr uint8_t DIR_PIN_B  = 25;
+constexpr uint8_t EN_PIN     = 14;
 
 // Driver UART
 constexpr uint8_t DRIVER_TX_PIN = 17;
@@ -19,12 +19,12 @@ constexpr uint8_t LCD_SDA_PIN = 21;
 constexpr uint8_t LCD_SCL_PIN = 22;
 
 // Rotary Encoder PinsConfig
-constexpr uint8_t ENCODER_DT_PIN = 26;
-constexpr uint8_t ENCODER_CLK_PIN = 27;
-constexpr uint8_t ENCODER_SW_PIN = 25;
+constexpr uint8_t ENCODER_DT_PIN = 35;
+constexpr uint8_t ENCODER_CLK_PIN = 34;
+constexpr uint8_t ENCODER_SW_PIN = 23;
 
 // buzzer
-constexpr uint8_t BUZZER_PIN = 14;
+constexpr uint8_t BUZZER_PIN = 32;
 
 // servo
-constexpr uint8_t SERVO_PIN = 13;
+constexpr uint8_t SERVO_PIN = 4;
